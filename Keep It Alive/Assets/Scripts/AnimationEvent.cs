3db356d39c6ff284baec7533e37ca9c72f38d847
Joeri Ridder <joeriridder@LIVE.NL>
2020-04-19 14:51:33 +0200

    Main Scene Title Menu Start Intro Cinematic Level Beginning Prologue

---
+++ Keep It Alive/Assets/Scripts/AnimationEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationEvent : MonoBehaviour
+{
+    [SerializeField] List<UnityEvent> events = new List<UnityEvent>();
+
+    public void InvokeEvent(int _index)
+    {
+        events[_index].Invoke();
+    }
+}
---
+++ Keep It Alive/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        startButton.gameObject.SetActive(false);
+        GameManager.Instance.StartIntroPan();
+    }
+}
